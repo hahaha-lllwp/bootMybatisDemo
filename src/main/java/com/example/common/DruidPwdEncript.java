1e818cdc20ffce05897f36532ec813ec340dbfe9
cimu <cimu@kmeila.com>
2018-09-28 11:53:43 +0800

    多数据源

--- src/main/java/com/example/common/DruidPwdEncript.java
+++ src/main/java/com/example/common/DruidPwdEncript.java
@@ -1,4 +1,9 @@
 package com.example.common;
 
+import com.alibaba.druid.filter.config.ConfigTools;
+
 public class DruidPwdEncript {
+    public static void main(String[] args) throws Exception {
+        ConfigTools.main(new String[]{"system"});
+    }
 }
